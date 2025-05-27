394de6da2fac79d5472a55ce1f1743b14fb008d6
Julia Lind <jl225vf@student.lnu.se>
2025-05-27 02:54:39 +0200

    Add pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import istanbul from 'vite-plugin-istanbul'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
@@ -19,6 +20,30 @@ export default defineConfig(({ command }) => ({
       extension: ['.js', '.vue'],
       cypress: true,
       requireEnv: false
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',  // Enable to automatically check for changes and update
+      manifest: {
+        name: 'SlimDreams',
+        short_name: 'SlimDreams',
+        theme_color: '#06b6d4',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/slimdreams/',
+        scope: '/slimdreams/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
   ],
   resolve: {
